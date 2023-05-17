496746a95414456909858152915cfeb004aed598
DanielBrMz <a01254805@tec.mx>
2023-05-16 20:40:52 -0700

    HomeScreen finished

--- custom-uber/src/screens/HomeScreen.tsx
+++ custom-uber/src/screens/HomeScreen.tsx
@@ -1,15 +1,49 @@
 import { StyleSheet, Text, View, Dimensions, ScrollView, Image, FlatList } from "react-native";
-import React from "react";
-import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
+import React, {useState, useRef, useEffect} from "react";
+import * as Location from "expo-location";
+import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
 import { colors, parameters } from "../global/styles";
+import {filterData, carsAround} from "../global/data";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { StatusBar } from "expo-status-bar";
-import { filterData } from "../global/data";
 import { mapStyle } from "../global/mapStyle";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const HomeScreen = (): JSX.Element => {
+
+  const [latLng, setLatLng] = useState<{latitude: number, longitude: number}>({latitude: 0, longitude: 0});
+
+  const checkPermission = async () => {
+    const hashPermission = await Location.requestForegroundPermissionsAsync();
+    if (hashPermission.status === "granted") {
+      const persmission = await askPermission();
+      return persmission;
+    }
+    return true;
+  }
+
+  const askPermission = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
+    return permission.status === "granted"
+  }
+
+  const getLocation = async () => {
+    try{
+      const {status}: {status: Location.PermissionStatus} = await Location.requestForegroundPermissionsAsync();
+      if(!status) return;
+
+      const {coords: {latitude, longitude}}: Location.LocationObject = await Location.getCurrentPositionAsync();
+
+      setLatLng({latitude, longitude});
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const _map = useRef<MapView>(null);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -136,11 +170,24 @@ const HomeScreen = (): JSX.Element => {
         </View>
         <Text style={styles.text4}> Around you </Text>
         <View style={{alignItems:"center", justifyContent:"center"}}>
-           {/*  <MapView 
+            <MapView
+                ref = {_map} 
                 provider={PROVIDER_GOOGLE}
                 style={styles.map} 
-                customMapStyle={mapStyle}>
-            </MapView> */}
+                customMapStyle={mapStyle}
+                showsUserLocation={true}
+                followsUserLocation={true}
+                region = {{...carsAround[0], latitudeDelta: 0.015, longitudeDelta: 0.0121}}
+                >
+                  {carsAround.map((item, index) => (
+                    <Marker coordinate = {item} key={index.toString()}>
+                      <Image source={require("../../assets/carMarker.png")}
+                             style={styles.carsAround}
+                             resizeMode="cover" 
+                      />
+                    </Marker>
+                  ))}
+            </MapView>
         </View>
       </ScrollView>
       <StatusBar style="light" backgroundColor="#2058c0" translucent={true} />
